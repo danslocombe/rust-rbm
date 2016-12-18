71222db59fd26dd5044a3ea75930e74761bd1810
Dan Slocombe <ds4314@ic.ac.uk>
2016-12-18 04:25:12 +0000

    Working rbm, produces distributions

--- src/main.rs
+++ src/main.rs
@@ -5,44 +5,88 @@ extern crate rand;
 mod rbm;
 
 use std::env;
+use std::io;
 
 use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
 
-type Input = Vec<u8>;
+pub type Input = Vec<u8>;
+pub type Label = u32;
+
+pub fn test() {
+}
 
 fn main() {
     
     let mut args = env::args();
 
     args.next();
-    let infile = match args.next(){
+    let train_data_file = match args.next(){
         Some(x) => x,
-        None => panic!("")
+        None => panic!("No specified input data file")
     };
+    /*
+    let train_label_file = match args.next(){
+        Some(x) => x,
+        None => panic!("No specified input labels")
+    };
+    */
     println!("Loading input from files...");
-    let inputs = inputsFromFile(&infile);
+    let inputs = inputs_from_file(&train_data_file);
+    //let labels = labels_from_file(&train_label_file);
     println!("Done");
+
+    println!("Enter number of hidden nodes: ");
+    let mut input_text = String::new();
+    io::stdin()
+        .read_line(&mut input_text)
+        .expect("failed to read from stdin");
+
+    let trimmed = input_text.trim();
+    let mut hidden_nodes = 10;
+    match trimmed.parse::<usize>() {
+        Ok(i)   => hidden_nodes = i,
+        Err(..) => println!("Failed to parse {}", input_text)
+    };
+    let mut rbm = rbm::create_rbm(inputs[0].len(), hidden_nodes);
+    for i in 1..100 {
+        println!("Epoch {}", i);
+        rbm::epoch(&mut rbm, &inputs);
+    }
+
+    println!("Sampling");
+    let sample = rbm::sample(&rbm);
+    println!("{:?}", sample);
 }
 
-fn inputsFromFile(filename : &String) -> Vec<Input> {
-    let f = readfile(&filename);
+fn inputs_from_file(filename : &String) -> Vec<Input> {
+    let f = read_file(&filename);
     let mut rdr = csv::Reader::from_string(f).has_headers(false);
 
     //  Convert strings to uint8s
     rdr.records().map(|r| {
         r.unwrap().iter().map(|x|{
             match x.parse::<u8>(){
-                Err(why) => panic!("Error in csv file, {}", filename),
+                Err(why) => panic!("Error in csv file, {}\n{}", filename, why),
                 Ok(a) => a,
             }
         }).collect()
     }).collect()
 }
 
-fn readfile(s : &String) -> String {
+fn labels_from_file(filename : &String) -> Vec<Label> {
+    let f = read_file(&filename);
+    f.split("\n").map(|x|{
+        match x.parse::<Label>(){
+            Err(why) => panic!("Error in label file, {}\n{}", filename, why),
+            Ok(a) => a,
+        }
+    }).collect::<Vec<Label>>()
+}
+
+fn read_file(s : &String) -> String {
     // Create a path to the desired file
     let path = Path::new(s);
     let display = path.display();
--- src/rbm.rs
+++ src/rbm.rs
@@ -1,5 +1,8 @@
 use rand::{thread_rng, Rng};
 
+use Input;
+use Label;
+
 use rulinalg::matrix::{Matrix, BaseMatrix};
 use rulinalg::vector::Vector;
 
@@ -12,23 +15,42 @@ pub struct RBM{
 }
 
 impl RBM{
-    fn propHidden (self, hidden : &Vector<f32>) -> Vector<f32> {
-        let mut vs = self.weights * hidden + self.vbias;
-        vs.iter_mut().map(|field| stepUpdate(field.to_owned()));
-        vs
+    fn prop_hidden (&self, hidden : &Vector<f32>) -> Vector<f32> {
+        let vs = &(self.weights) * hidden + &(self.vbias);
+        Vector::from(
+            vs.iter().map(
+                |field| sigmoid(field.to_owned())).
+            collect::<Vec<f32>>()
+        )
     }
-    fn propVisible (self, visible : &Vector<f32>) -> Vector<f32> {
-        let mut hs = self.weights.transpose() * visible + self.hbias;
-        hs.iter_mut().map(|field| stepUpdate(field.to_owned()));
-        hs
+    fn prop_visible (&self, visible : &Vector<f32>) -> Vector<f32> {
+        let vs = &(self.weights.transpose()) * visible + &(self.hbias);
+        Vector::from(
+            vs.iter().map(
+                |field| sigmoid(field.to_owned())).
+            collect::<Vec<f32>>()
+        )
     }
 }
 
-fn stepUpdate(x : f32) -> f32 {
+fn rand_vec(size : usize) -> Vec<f32> {
+    let mut v = Vec::new();
+    for i in 0..(size) {
+        v.push(thread_rng().gen_range(0.0, 1.0));
+    }
+    v
+}
+
+fn step_update(x : f32) -> f32 {
     if x > 0.0 {1.0} else {0.0}
 }
 
-pub fn randomiseWeightMatrix (vsize : usize, hsize : usize) -> Matrix<f32> {
+fn sigmoid (x : f32) -> f32{
+    1.0 / (1.0 + (-x).exp())
+}
+
+
+pub fn randomise_weight_matrix(vsize : usize, hsize : usize) -> Matrix<f32> {
     let mut ws_values = Vec::new();
     for i in 0..(vsize * hsize) {
         ws_values.push(thread_rng().gen_range(0.0, 1.0));
@@ -37,7 +59,67 @@ pub fn randomiseWeightMatrix (vsize : usize, hsize : usize) -> Matrix<f32> {
     Matrix::new(vsize, hsize, ws_values)
 }
 
-pub fn createRBM (vsize : usize, hsize : usize, wm : &mut Matrix<f32>) -> RBM {
+//fn epoch (rbm : &mut RBM, batch : &Vec<Input>, labels : &Vec<Label>){
+pub fn epoch (rbm : &mut RBM, batch : &Vec<Input>){
+
+    let mut d_weights : Matrix<f32> = Matrix::zeros(rbm.visible_size, rbm.hidden_size);
+    let mut d_vbiases : Vector<f32> = Vector::zeros(rbm.visible_size);
+    let mut d_hbiases : Vector<f32> = Vector::zeros(rbm.hidden_size);
+
+
+    for v_batch in batch{
+
+        //  Convert to Vec of f32
+        let v_batch1 = v_batch.iter().map(|x| x.to_owned() as f32).collect::<Vec<f32>>();
+
+        //  Contrastive divergence, for now k = 1
+        let v0 = Vector::from(v_batch1);
+        let h0 = rbm.prop_visible(&v0);
+        let vk = rbm.prop_hidden(&h0);
+        let hk = rbm.prop_visible(&vk);
+
+
+        for i in 1..rbm.visible_size{
+            for j in 1..rbm.hidden_size{
+                d_weights[[i, j]] += h0[j] * v0[i] - hk[j] * vk[i];
+                d_vbiases[i] += v0[i] - vk[i];
+                d_hbiases[j] += h0[j] - hk[j];
+            }
+        }
+
+    }
+
+    let learning_rate = 0.05;
+    let ll2 = 0.01;
+    let v = 0.01;
+
+    let batch_size = batch.len() as f32;
+    let lr = learning_rate / batch_size;
+
+    for i in 1..rbm.visible_size{
+        for j in 1..rbm.hidden_size{
+            //  TODO adjust based on d_weights derivitive
+            rbm.weights[[i, j]] += lr * d_weights[[i, j]] - ll2 * rbm.weights[[i, j]];
+            rbm.vbias[i] += lr * d_vbiases[i] - ll2 * rbm.vbias[i];
+            rbm.hbias[j] += lr * d_hbiases[j] - ll2 * rbm.hbias[j];
+        }
+    }
+}
+
+pub fn sample(rbm : &RBM) -> Vector<f32> {
+    //  Create random vector
+    let mut v = Vector::from(rand_vec(rbm.visible_size));
+    let mut h = rbm.prop_visible(&v);
+
+    let mcmcs = 32;
+    for i in 1..mcmcs {
+        h = rbm.prop_visible(&v);
+        v = rbm.prop_hidden(&h);
+    }
+    v
+}
+
+pub fn create_rbm(vsize : usize, hsize : usize) -> RBM {
 
     let mut vb = Vec::new();
     for i in 0..vsize {
@@ -46,13 +128,13 @@ pub fn createRBM (vsize : usize, hsize : usize, wm : &mut Matrix<f32>) -> RBM {
 
     let mut hb = Vec::new();
     for i in 0..hsize {
-        vb.push(thread_rng().gen_range(-1.0, 1.0));
+        hb.push(thread_rng().gen_range(-1.0, 1.0));
     }
 
     RBM {
         visible_size : vsize,
         hidden_size  : hsize,
-        weights      : randomiseWeightMatrix(vsize, hsize),
+        weights      : randomise_weight_matrix(vsize, hsize),
         vbias        : Vector::new(vb),
         hbias        : Vector::new(hb),
     }
