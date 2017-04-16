81d755b81fb73aa3f646863747e8411c58bc1da0
Daniel Slocombe <ds4314@ic.ac.uk>
2017-04-16 18:34:01 +0100

    Add labels

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,19 @@
 extern crate csv;
 extern crate rulinalg;
 extern crate rand;
+extern crate kronecker;
 
 mod rbm;
 
 use std::env;
 use std::io;
-
 use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
 use std::result;
 use std::fmt;
+use kronecker::delta;
 
 pub struct RBMError(String);
 impl fmt::Display for RBMError {
@@ -64,21 +65,23 @@ fn try_main() -> Result<()> {
     args.next();
     let train_data_file = args.next().ok_or("No specified input data file")?;
     let train_label_file = args.next().ok_or("No specified input labels")?;    
-    println!("Loading input from files...");
 
+    println!("Loading inputs from files...");
     let inputs = inputs_from_file(&train_data_file)?;
+
+    println!("Loading inputs from files...");
     let raw_labels : Vec<usize>= labels_from_file(&train_label_file)?;
 
     //  Parse labels
     let max_label = raw_labels.iter()
-                          .map(|&x| x as usize)
-                          .fold(0, |x, y| if x > y {x} else {y});
+                              .map(|&x| x as Label)
+                              .fold(0, |x, y| if x > y {x} else {y});
 
     //  TODO map with partially applied 'parse label'
     let labels : Vec<Input> = raw_labels.iter().map(|&x| {
         let mut v : Input = Vec::new();
-        for i in LABEL_START..max_label{
-            v.push(if i == (x as usize) {1} else {0} as InPrim);
+        for i in LABEL_START..max_label + 1{
+            v.push(delta(&i, &(x as usize)));
         }
         v
     }).collect();
@@ -86,14 +89,13 @@ fn try_main() -> Result<()> {
 
     let hidden_nodes = from_stdin!("Enter number of hidden nodes: ", usize, 10) as usize;
 
-    let mut rbm = rbm::create_rbm(inputs[0].len(), hidden_nodes, max_label);
+    let mut rbm = rbm::new(inputs[0].len(), hidden_nodes, max_label);
     for i in 1..100 {
         //  TODO generate minibatch
         //  ie. generate a random subset
         let batch = &inputs;
         let batch_labels = &labels;
 
-        println!("Epoch {}", i);
         rbm.epoch(batch, batch_labels);
     }
 
@@ -110,7 +112,7 @@ fn try_main() -> Result<()> {
 
 fn parse_label(label : usize, max_label : usize) -> Input{
     let mut v : Input = Vec::new();
-    for i in LABEL_START..max_label{
+    for i in LABEL_START..max_label + 1 {
         v.push(if i == (label as usize) {1} else {0} as InPrim);
     }
     v
@@ -133,9 +135,9 @@ fn inputs_from_file(filename : &String) -> Result<Vec<Input>> {
 
 fn labels_from_file(filename : &String) -> Result<Vec<Label>> {
     let f = read_file(&filename)?;
-    f.split("\n").map(|x|{
+    f.split("\n").filter(|x| !x.is_empty()).map(|x|{
         x.parse::<Label>().map_err(|why| {
-            RBMError(format!("Error in label file, {}\n{}", filename, why))
+            RBMError(format!("Malformed label file, {}\n{}", filename, why))
         })
     }).collect()
 }
--- src/rbm.rs
+++ src/rbm.rs
@@ -16,9 +16,11 @@ pub struct RBM{
     hidden_size   : usize, 
     labels        : usize, 
     weights       : Matrix<f32>,
-    label_weights : Matrix<f32>,
     vbias         : Vector<f32>,
     hbias         : Vector<f32>,
+
+    //  Number of monte carlo marcov chain iterations to use when sampling
+    sample_mcmc   : usize,
 }
 
 impl RBM{
@@ -26,7 +28,7 @@ impl RBM{
         let vs = &(self.weights) * hidden + &(self.vbias);
         Vector::from(
             vs.iter().map(
-                |field| sigmoid(field.to_owned())).
+                |field| sigmoid(field)).
             collect::<Vec<f32>>()
         )
     }
@@ -34,25 +36,30 @@ impl RBM{
         let vs = &(self.weights.transpose()) * visible + &(self.hbias);
         Vector::from(
             vs.iter().map(
-                |field| sigmoid(field.to_owned())).
+                |field| sigmoid(field)).
             collect::<Vec<f32>>()
         )
     }
 
-    pub fn epoch (&mut self, batch : &Vec<Input>, batch_labels : &Vec<Input>){
+    pub fn epoch (&mut self, batch : &[Input], labels : &[Input]){
 
-        let mut d_weights : Matrix<f32> = Matrix::zeros(self.visible_size, self.hidden_size);
-        let mut d_vbiases : Vector<f32> = Vector::zeros(self.visible_size);
+
+        //  Construct delta values
+        let mut d_weights : Matrix<f32> = Matrix::zeros(self.labels + self.visible_size, self.hidden_size);
+        let mut d_vbiases : Vector<f32> = Vector::zeros(self.labels + self.visible_size);
         let mut d_hbiases : Vector<f32> = Vector::zeros(self.hidden_size);
 
+        let batch_labels = batch.iter().zip(labels.iter());
 
-        for v_batch in batch{
+        for (example, label) in batch_labels {
 
-            //  Convert to Vec of f32
-            let v_batch1 = v_batch.iter().map(|x| x.to_owned() as f32).collect::<Vec<f32>>();
+            //  Convert example and label into one visible layer Vector
+            let v0 = Vector::from(example.iter()
+                                          .chain(label.iter())
+                                          .map(|x| *x as f32)
+                                          .collect::<Vec<_>>());
 
-            //  Contrastive divergence, for now k = 1
-            let v0 = Vector::from(v_batch1);
+            //  Contrastive divergence, for k = 1
             let h0 = self.prop_visible(&v0);
             let vk = self.prop_hidden(&h0);
             let hk = self.prop_visible(&vk);
@@ -78,14 +85,16 @@ impl RBM{
 
         let learning_rate = 0.05;
         let ll2 = 0.01;
+
+        //  TODO Add Momentum
         let v = 0.01;
+        //
 
         let batch_size = batch.len() as f32;
         let lr = learning_rate / batch_size;
 
         for i in 1..self.visible_size{
             for j in 1..self.hidden_size{
-                //  TODO adjust based on d_weights derivitive
                 self.weights[[i, j]] += lr * d_weights[[i, j]] - ll2 * self.weights[[i, j]];
             }
         }
@@ -100,16 +109,22 @@ impl RBM{
     }
 
     pub fn sample(&self, clamped_labels : Input) -> Vector<f32> {
+        let input : Vec<f32> = rand_vec(self.visible_size).iter().cloned()
+                              .chain(clamped_labels.iter()
+                                     .map(|x| *x as f32))
+                              .collect();
 
-        let mut v = Vector::from(rand_vec(self.visible_size));
+        let mut v = Vector::from(input);
         let mut h = self.prop_visible(&v);
 
-        let mcmcs = 32;
-        for _ in 1..mcmcs {
+        for _ in 1..self.sample_mcmc {
             h = self.prop_visible(&v);
-            v = self.prop_hidden(&h);
+            v = Vector::from(self.prop_hidden(&h).iter().take(self.visible_size).cloned()
+                              .chain(clamped_labels.iter()
+                                     .map(|x| *x as f32))
+                              .collect::<Vec<_>>());
         }
-        v
+        Vector::from(v.iter().take(self.visible_size).cloned().collect::<Vec<_>>())
     }
 }
 
@@ -125,7 +140,7 @@ fn step_update(x : f32) -> f32 {
     if x > 0.0 {1.0} else {0.0}
 }
 
-fn sigmoid (x : f32) -> f32{
+fn sigmoid (x : &f32) -> f32{
     1.0 / (1.0 + (-x).exp())
 }
 
@@ -140,10 +155,10 @@ pub fn randomise_weight_matrix(vsize : usize, hsize : usize) -> Matrix<f32> {
 }
 
 
-pub fn create_rbm(vsize : usize, hsize : usize, labels : usize) -> RBM {
+pub fn new(vsize : usize, hsize : usize, labels : usize) -> RBM {
 
     let mut vb = Vec::new();
-    for _ in 0..vsize {
+    for _ in 0..vsize + labels {
         vb.push(thread_rng().gen_range(-1.0, 1.0));
     }
 
@@ -156,9 +171,10 @@ pub fn create_rbm(vsize : usize, hsize : usize, labels : usize) -> RBM {
         visible_size  : vsize,
         hidden_size   : hsize,
         labels        : labels,
-        weights       : randomise_weight_matrix(vsize, hsize),
-        label_weights : randomise_weight_matrix(labels, hsize),
+        weights       : randomise_weight_matrix(vsize + labels, hsize),
+        //label_weights : randomise_weight_matrix(labels, hsize),
         vbias         : Vector::new(vb),
         hbias         : Vector::new(hb),
+        sample_mcmc   : 32,
     }
 }
